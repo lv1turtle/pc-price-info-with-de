66c1c0b398a0ad2000a7c029d87a9edc62378305
lv1.turtle <skqltldnjf77@gmail.com>
2025-06-08 23:51:35 +0900

    trouble shooting - install
    Working on resolving the issue
    - error : helm installation failed
    - https://invited-spur-391.notion.site/1-helm-install-20cbfc82bca380d8958fd71f6733ace2

--- setting.sh
+++ setting.sh
@@ -1,3 +1,7 @@
+# minikube start
+#----------------------------------------------------
+minikube start --driver=docker --memory=12288 --cpus=4
+
 # Spark
 
 docker build -t my-spark .
@@ -16,6 +20,13 @@ helm repo update
 # cluster 내에 airflow 환경 생성
 kubectl create namespace airflow
 
+# minkube 내로 dag 및 data 전송
+tar -czf shared.tar.gz -C ./shared .
+minikube cp shared.tar.gz /opt/shared.tar.gz
+minikube ssh -- "tar -xzf /opt/shared.tar.gz -C /opt && rm /opt/shared.tar.gz"
+rm shared.tar.gz
+
+
 # pvc 등록
 kubectl apply -f airflow/airflow-pvc.yaml
 
@@ -23,6 +34,9 @@ helm install airflow apache-airflow/airflow \
   -n airflow \
   -f airflow/airflow-values.yaml \
   -f airflow/secrets.yaml
+
+# install 실패시 삭제
+# helm uninstall airflow -n airflow
 
 # check status
 kubectl get pods -n airflow
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-minikube start --driver=docker
+minikube start --driver=docker --memory=12288 --cpus=4
+
+# minkube 내로 dag 및 data 전송
+minikube cp ./shared /opt/shared
